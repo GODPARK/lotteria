ebe9478b791db751b57d4c3c76340910c4762908
Jung Hyun Park <rootjh937@gmail.com>
2021-09-01 18:35:27 +0900

    lotteria api init

---
+++ api/src/main/java/com/lotteria/api/entity/lotto/LottoHistory.java
@@ -0,0 +1,55 @@
+package com.lotteria.api.entity.lotto;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Positive;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "lotto_history")
+@Builder @Getter @Setter @ToString
+@NoArgsConstructor @AllArgsConstructor
+public class LottoHistory {
+
+    @Positive
+    @Id
+    @Column(name = "round_num")
+    private long roundNum;
+
+    @Positive @Max(value = 45)
+    @Column(name = "num1", nullable = false)
+    private int num1;
+
+    @Positive @Max(value = 45)
+    @Column(name = "num2", nullable = false)
+    private int num2;
+
+    @Positive @Max(value = 45)
+    @Column(name = "num3", nullable = false)
+    private int num3;
+
+    @Positive @Max(value = 45)
+    @Column(name = "num4", nullable = false)
+    private int num4;
+
+    @Positive @Max(value = 45)
+    @Column(name = "num5", nullable = false)
+    private int num5;
+
+    @Positive @Max(value = 45)
+    @Column(name = "num6", nullable = false)
+    private int num6;
+
+    @Positive @Max(value = 45)
+    @Column(name = "bonus", nullable = false)
+    private int bonus;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+
+    @Transient
+    private List<Integer> numberList;
+}
---
+++ api/src/main/java/com/lotteria/api/entity/lotto/LottoStatistics.java
@@ -0,0 +1,4 @@
+package com.lotteria.api.entity.lotto;
+
+public class LottoStatistics {
+}
